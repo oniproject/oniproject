7d834ece2bbd51a31df6acdb3b0a3cd2903740dd
Lain-dono <lain.dono@gmail.com>
2014-06-08 09:50:54 +0400

    init main.go

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"flag"
+	"log"
+)
+
+var addr = flag.String("addr", ":8000", "http address")
+var rpc = flag.String("rpc", ":7000", "rpc address")
+var master = flag.Bool("master", false, "this is a master")
+var game = flag.Bool("game", false, "this is a game mechanic")
+var database = flag.Bool("db", false, "this is a database")
+
+func main() {
+	flag.Parse()
+	switch {
+	case *master:
+		// TODO run master
+		log.Println("run MASTER:", *addr, "rpc:", *rpc)
+	case *game:
+		// TODO run game
+		log.Println("run GAME:", *addr, "rpc:", *rpc)
+	case *database:
+		// TODO run database
+		log.Println("run DATABASE:", *addr, "rpc:", *rpc)
+	default:
+		// TODO run all
+		log.Println("run ALL:", *addr, "rpc:", *rpc)
+	}
+}
