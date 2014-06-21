1d728634fc6de73fb4e72660fd2d391b91364f6d
Lain-dono <lain.dono@gmail.com>
2014-06-21 22:16:34 +0400

    use strict

--- js/avatar.js
+++ js/avatar.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var Isomer = require('isomer');
 
 var Octahedron = require('./octahedron');
--- js/stairs.js
+++ js/stairs.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var Isomer = require('isomer');
 
 /* Some convenient renames */
