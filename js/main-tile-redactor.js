57acf6aae59ae3472f449ae052f902ddc340b06b
Lain-dono <lain.dono@gmail.com>
2014-08-14 22:43:51 +0400

    fix redactor

--- js/main-tile-redactor.js
+++ js/main-tile-redactor.js
@@ -73,17 +73,6 @@ for(var y=0, ml=data.length; y<ml; y++) {
 	}
 }
 
-window.type = {
-	layer: 'second', t: 5, v:5, auto:undefined,
-}
-
-stage.click = function(event) {
-	var loc = event.getLocalPosition(scene);
-	var x = loc.x/32|0;
-	var y = loc.y/32|0;
-	scene.setAt(x, y, type.layer, type.t, type.v, type.auto);
-	console.log(x, y, type);
-}
 
 stage.addChild(scene);
 
@@ -134,4 +123,151 @@ requestAnimFrame(animate);
 function animate() {
 	renderer.render(stage);
 	requestAnimFrame(animate);
+}
+
+
+
+
+
+
+
+var type = new Vue({
+	el: '#palete',
+	data: {
+		layer:'first',
+		t: 0,
+		v: 0,
+		auto: false,
+		//{ img: "", x: 0, y:0 },
+		A1: [],
+		A2: [],
+		A3: [],
+		A4: [],
+		A5: [],
+		B: [],
+		C: [],
+		D: [],
+		E: [],
+	},
+	methods: {
+		setActive: function(a, i){
+			console.log(a, i);
+			switch(a) {
+			case 'A1':
+				this.$data.auto = true;
+				this.$data.t = 0;
+				if(i%2) {
+					i-=1;
+					i*=2;
+					this.$data.v = i+3;
+					//this.$data.layer = 'second';
+				} else {
+					i*=2;
+					this.$data.v = [i, i+1, i+2];
+					//this.$data.layer = 'first';
+				}
+				break;
+			case 'A2':
+				this.$data.auto = true;
+				this.$data.t = 1;
+				this.$data.v = i;
+				//this.$data.layer = 'first';
+				break;
+			case 'A3':
+				this.$data.auto = false;
+				this.$data.t = 2;
+				this.$data.v = i;
+				//this.$data.layer = 'first';
+				break;
+			case 'A4':
+				this.$data.auto = false;
+				this.$data.t = 3;
+				this.$data.v = i;
+				//this.$data.layer = 'first';
+				break;
+			case 'A5':
+				this.$data.auto = false;
+				this.$data.t = 4;
+				this.$data.v = i;
+				//this.$data.layer = 'first';
+				break;
+			case 'B':
+				this.$data.auto = false;
+				this.$data.t = 5;
+				this.$data.v = i;
+				//this.$data.layer = 'first';
+				break;
+			case 'C':
+				this.$data.auto = false;
+				this.$data.t = 6;
+				this.$data.v = i;
+				//this.$data.layer = 'first';
+				break;
+			case 'D':
+				this.$data.auto = false;
+				this.$data.t = 7;
+				this.$data.v = i;
+				//this.$data.layer = 'first';
+				break;
+			case 'E':
+				this.$data.auto = false;
+				this.$data.t = 8;
+				this.$data.v = i;
+				//this.$data.layer = 'first';
+				break;
+			}
+		},
+	},
+});
+
+setTimeout(function() {
+	for(var y=0; y<10; y+=3) {
+		type.$data.A1.push({x:0, y:-y, img: Outside[0].image.texture.baseTexture.source.src});
+		type.$data.A1.push({x:-6, y:-y, img: Outside[0].image.texture.baseTexture.source.src});
+		type.$data.A1.push({x:-8, y:-y, img: Outside[0].image.texture.baseTexture.source.src});
+		type.$data.A1.push({x:2, y:-y, img: Outside[0].image.texture.baseTexture.source.src});
+	}
+	for(var y=0; y<10; y+=3) {
+		type.$data.A2.push({x:0, y:-y, img: Outside[1].image.texture.baseTexture.source.src});
+		type.$data.A2.push({x:-2, y:-y, img: Outside[1].image.texture.baseTexture.source.src});
+		type.$data.A2.push({x:-4, y:-y, img: Outside[1].image.texture.baseTexture.source.src});
+		type.$data.A2.push({x:-6, y:-y, img: Outside[1].image.texture.baseTexture.source.src});
+		type.$data.A2.push({x:-8, y:-y, img: Outside[1].image.texture.baseTexture.source.src});
+		type.$data.A2.push({x:-10, y:-y, img: Outside[1].image.texture.baseTexture.source.src});
+		type.$data.A2.push({x:-12, y:-y, img: Outside[1].image.texture.baseTexture.source.src});
+		type.$data.A2.push({x:-14, y:-y, img: Outside[1].image.texture.baseTexture.source.src});
+	}
+	for(var y=0; y<8; y++) {
+		for(var x=0; x<16; x++) {
+			type.$data.A3.push({x:-x, y:-y, img: Outside[2].image.texture.baseTexture.source.src});
+		}
+	}
+	for(var y=0; y<15; y++) {
+		for(var x=0; x<16; x++) {
+			type.$data.A4.push({x:-x, y:-y, img: Outside[3].image.texture.baseTexture.source.src});
+		}
+	}
+	for(var y=0; y<16; y++) {
+		for(var x=0; x<8; x++) {
+			type.$data.A5.push({x:-x, y:-y, img: Outside[4].image.texture.baseTexture.source.src});
+		}
+	}
+	for(var y=0; y<16; y++) {
+		for(var x=0; x<16; x++) {
+			type.$data.B.push({x:-x, y:-y, img: Outside[5].image.texture.baseTexture.source.src});
+		}
+	}
+	for(var y=0; y<16; y++) {
+		for(var x=0; x<16; x++) {
+			type.$data.C.push({x:-x, y:-y, img: Outside[6].image.texture.baseTexture.source.src});
+		}
+	}
+}, 3000);
+
+stage.click = function(event) {
+	var loc = event.getLocalPosition(scene);
+	var x = loc.x/32|0;
+	var y = loc.y/32|0;
+	scene.setAt(x, y, type.$data.layer, type.$data.t, type.$data.v, type.$data.auto);
+	console.log(x, y, type.$data);
 }
