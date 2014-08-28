f2d9ff511adbb5f1fe5816a65f04179c7ffd1d20
Lain-dono <lain.dono@gmail.com>
2014-08-28 11:45:41 +0400

    fix build and rm ublic/map.html

--- gulpfile.js
+++ gulpfile.js
@@ -10,10 +10,11 @@ var browserify = require('browserify'),
 
 var dest = './public/';
 var apps = {
-	game:      {dest: 'main.js',      source: './js/main.js'},
-	redactor:  {dest: 'redactor.js',  source: './redactor/map/main.js'},
-	tredactor: {dest: 'tredactor.js', source: './redactor/tile/main.js'},
-	a2d:       {dest: 'a2d.js',       source: './redactor/a2d/main.js'},
+	game:     {dest: 'main.js',     source: './js/main.js'},
+	map:      {dest: 'map.js',      source: './redactor/map/main.js'},
+	tile:     {dest: 'tile.js',     source: './redactor/tile/main.js'},
+	a2d:      {dest: 'a2d.js',      source: './redactor/a2d/main.js'},
+	redactor: {dest: 'redactor.js', source: './redactor/main.js'},
 }
 
 function handleErrors(err) {
@@ -25,11 +26,8 @@ function handleErrors(err) {
 var fn = function(app, isWatching) {
 	return function() {
 		var bundler = browserify({
-			// Required watchify args
 			cache: {}, packageCache: {}, fullPaths: true,
-			// Browserify Options
 			entries: [app.source],
-			//extensions: ['.coffee', '.hbs', ],
 			debug: true
 		});
 
@@ -63,14 +61,22 @@ for(var k in apps) {
 gulp.task('watch', w);
 gulp.task('build', b);
 
-gulp.task('default', ['watch']);
-
-/* reload */
 gulp.task('server', function(next) {
-	var connect = require('connect'),
-		server = connect();
-	server.use(connect.static('./')).listen(process.env.PORT || 9999, next);
+	var connect = require('connect');
+	connect()
+		.use(connect.static('./'))
+		.use(connect.static('public'))
+		.use(connect.directory('public'))
+		.use(function(req, res) {
+			if(req.method == 'POST') {
+				console.log(req.body);
+			} else {
+				res.end('FUCK U\n');
+			}
+		})
+		.listen(process.env.PORT || 9000, next);
 });
+
 gulp.task('reload', ['server'], function() {
 	var server = livereload();
 	gulp.watch('public/**').on('change', function(event) {
@@ -78,3 +84,5 @@ gulp.task('reload', ['server'], function() {
 		console.log('File '+event.path+' was '+event.type+', running tasks...');
 	});
 });
+
+gulp.task('default', ['watch']);
