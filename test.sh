4f6ec4f06d206cd2d561542ace67b061035ff8fb
Lain-dono <lain.dono@gmail.com>
2014-10-17 16:05:13 +0400

    fix test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-mockgen -package="mock"  oniproject/oni/game EffectReceiver,FeatureReceiver,SkillTarget > oni/game/mock/mock.go
-go test ./oni... -v
+mockgen -package="mock"  oniproject/oni/game EffectReceiver,FeatureReceiver,SkillTarget,Walkabler > oni/game/mock/mock.go
 
-go test -coverprofile=coverage.out ./oni/game
+#go test ./oni... -v
+#go test ./oni/game -v
+
+echo COVER
+go test -coverprofile=coverage.out ./oni/game -v
 go tool cover -html=coverage.out
 rm -f coverage.out
