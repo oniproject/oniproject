1fc9ef4ba1cd57566f60b21e5f12f01c40b1a8ed
Lain-dono <lain.dono@gmail.com>
2014-11-10 03:46:32 +0300

    upd public/main.js

--- public/main.js
+++ public/main.js
@@ -133,6 +133,7 @@ var UI = window.UI = new Vue({
 			},
 		],
 		target: {
+			Id: 0,
 			Race: 0,
 			HP: 0,
 			MHP: 0,
@@ -208,7 +209,7 @@ var UI = window.UI = new Vue({
 	},
 	computed: {
 		showTargetBar: function() {
-			return !!this.target.MHP;
+			return !!this.target.Id;
 		},
 	},
 	components: {
